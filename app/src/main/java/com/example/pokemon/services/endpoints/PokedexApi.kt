c12681ad91d58a7bddb2490545b619cb413761b4
Kar2C <kycarrillo@uniboyaca.edu.co>
2024-11-21 16:05:13 -0500

    Se agrego la funcionalidad para ver los pokemones que hay en la región de Kanto (se eliminaron los estilos existentes)

--- app/src/main/java/com/example/pokemon/services/endpoints/PokedexApi.kt
+++ app/src/main/java/com/example/pokemon/services/endpoints/PokedexApi.kt
@@ -1,6 +1,5 @@
 package com.example.pokemon.services.endpoints
 
-import com.example.pokemon.services.models.PokemonDetails
 import com.example.pokemon.services.models.PokemonResponse
 import com.example.pokemon.services.models.RegionResponse
 import retrofit2.Call
@@ -9,14 +8,11 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PokedexApi {
-    @GET("pokedex/")
-    suspend fun getPokedex(): Response<PokemonResponse>
-
-    @GET("pokemon/{id}/")  // Añadir esta función para obtener detalles de cada Pokémon
-    suspend fun getPokemonDetails(@Path("id") id: Int): Response<PokemonDetails>
-
     @GET("region/")
     suspend fun getRegions(): RegionResponse
+
+    @GET("pokedex/kanto/")
+    suspend fun getPokedex(): Response<PokemonResponse>
 }
 
 data class RegionResponse(val results: List<Region>)
